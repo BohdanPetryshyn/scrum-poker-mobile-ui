c079820b8aaef7227b92ad554a41c8442a847984
BohdanPetryshyn <b.y.petryshyn@gmail.com>
2020-04-17 12:24:21 +0300

    Add SessionNavigator tab buttons

--- navigations/SessionNavigator.js
+++ navigations/SessionNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import VoteScreen from '../screens/VoteScreen';
 import ResultsScreen from '../screens/ResultsScreen';
@@ -8,8 +9,24 @@ const Tab = createBottomTabNavigator();
 
 const SessionNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Vote" component={VoteScreen} />
-    <Tab.Screen name="Results" component={ResultsScreen} />
+    <Tab.Screen
+      name="Vote"
+      component={VoteScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="ios-podium" size={size + 10} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Results"
+      component={ResultsScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="ios-list" size={size + 10} color={color} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
