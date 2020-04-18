07f83bd30299c4a320d1d908909e08747b591157
BohdanPetryshyn <b.y.petryshyn@gmail.com>
2020-04-18 17:40:13 +0300

    Add required data fetching before app startup

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 
 import Navigator from './navigations';
 import store from './store';
 import LoadExpoFonts from './components/hocs/LoadExpoFonts';
+import { fetchCardSchemas } from './store/actions/requestActions';
 
 export default () => {
+  useEffect(() => {
+    store.dispatch(fetchCardSchemas());
+  });
+
   return (
     <LoadExpoFonts>
       <Provider store={store}>
