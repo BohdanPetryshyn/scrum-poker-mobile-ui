e7d29c7b70aeb1fef232b15dfc129979364409f5
BohdanPetryshyn <b.y.petryshyn@gmail.com>
2020-04-21 20:54:52 +0300

    Add message to the Waiting screen

--- components/VotingScreen/WaitingScreen/index.js
+++ components/VotingScreen/WaitingScreen/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Container, Content, Text } from 'native-base';
+import { Container, Content, Grid, H2, Row } from 'native-base';
 
 const WaitingScreen = () => {
   return (
     <Container>
-      <Content>
-        <Text>Wait...</Text>
+      <Content contentContainerStyle={{ flex: 1 }}>
+        <Grid>
+          <Row style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <H2 style={{ textAlign: 'center' }}>
+              Host creating a story.
+              {'\n'}
+              Please wait...
+            </H2>
+          </Row>
+        </Grid>
       </Content>
     </Container>
   );
