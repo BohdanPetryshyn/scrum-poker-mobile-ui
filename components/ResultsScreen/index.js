a01cbc805428d548f7c6d0e40e9017150c88cb9e
BohdanPetryshyn <b.y.petryshyn@gmail.com>
2020-05-10 11:11:38 +0300

    Remove unnecessary header from ResultsScreen

--- components/ResultsScreen/index.js
+++ components/ResultsScreen/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { Container, Content, Grid, Row } from 'native-base';
-import { scaleSize } from '../styles/size';
-import StoryHeader from '../StoryHeader';
+import { Container, Content } from 'native-base';
 import VotingResults from './VotingResults';
 
 const ResultsScreen = () => {
   return (
     <Container>
-      <Content contentContainerStyle={{ flex: 1, margin: scaleSize(10) }}>
-        <Grid>
-          <Row size={1}>
-            <StoryHeader />
-          </Row>
-          <Row size={8}>
-            <VotingResults />
-          </Row>
-        </Grid>
+      <Content>
+        <VotingResults />
       </Content>
     </Container>
   );
