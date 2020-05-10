f97b6126f2e3a489942ce91b69c4f220f8aa61f5
BohdanPetryshyn <b.y.petryshyn@gmail.com>
2020-05-10 10:17:38 +0300

    Add Header and grid structure to ResultsScreen

--- components/ResultsScreen/index.js
+++ components/ResultsScreen/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Container, Content, Text } from 'native-base';
+import { Container, Content, Grid, Row } from 'native-base';
+import { scaleSize } from '../styles/size';
+import StoryHeader from '../StoryHeader';
 
 const ResultsScreen = () => {
   return (
     <Container>
-      <Content>
-        <Text>Results</Text>
+      <Content contentContainerStyle={{ flex: 1, margin: scaleSize(10) }}>
+        <Grid>
+          <Row size={1}>
+            <StoryHeader />
+          </Row>
+          <Row size={8}></Row>
+        </Grid>
       </Content>
     </Container>
   );
