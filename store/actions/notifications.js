b645227907725bc80afde23b7ca874b2a4f4fe80
BohdanPetryshyn <b.y.petryshyn@gmail.com>
2020-05-27 10:52:57 +0300

    Dispatch notification as an redux action when it was selected from os or Toast was tapped

--- store/actions/notifications.js
+++ store/actions/notifications.js
@@ -1,9 +1,12 @@
 import { Notifications } from 'expo';
+import { Toast } from 'native-base';
+
 import { requestNotificationsPermission } from '../../permissions/notifications';
 
 export const NOTIFICATIONS_ACTIONS = {
   PERMISSION_DENIED: 'PERMISSION_DENIED',
   TOKEN_RETRIEVED: 'TOKEN_RETRIEVED',
+  NOTIFICATION_RECEIVED: 'NOTIFICATION_RECEIVED',
 };
 
 const permissionDenied = () => ({
@@ -15,11 +18,44 @@ const tokenRetrieved = token => ({
   payload: token,
 });
 
+const notificationReceived = notification => ({
+  type: NOTIFICATIONS_ACTIONS.NOTIFICATION_RECEIVED,
+  payload: notification,
+});
+
+const showToast = (title, body, onSelect) => {
+  Toast.show({
+    text: `${title}\n${body}`,
+    buttonText: 'Okay',
+    position: 'top',
+    duration: 4000,
+    onClose: reason => reason === 'user' && onSelect(),
+  });
+};
+
+const getNotificationHandler = dispatch => notification => {
+  const {
+    origin,
+    data: { title, body },
+  } = notification;
+
+  const onSelect = () => dispatch(notificationReceived(notification));
+
+  if (origin === 'received') {
+    showToast(title, body, onSelect);
+  } else {
+    onSelect();
+  }
+};
+
 export const registerForPushNotifications = () => async dispatch => {
   const notificationPermissionStatus = await requestNotificationsPermission();
   if (!notificationPermissionStatus) {
     return dispatch(permissionDenied());
   }
+
+  Notifications.addListener(getNotificationHandler(dispatch));
+
   const token = await Notifications.getExpoPushTokenAsync();
   return dispatch(tokenRetrieved(token));
 };
