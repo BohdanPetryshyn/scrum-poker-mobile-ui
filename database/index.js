6c476d8bf036301fe7724bd544a5e1666cb70ed2
BohdanPetryshyn <b.y.petryshyn@gmail.com>
2020-05-31 18:18:09 +0300

    Define FETCH_PAST_SESSIONS task

--- database/index.js
+++ database/index.js
@@ -3,7 +3,11 @@ import * as SQLite from 'expo-sqlite';
 import assertTablesWithDb from './assertTablesWithDb';
 import { createSessionWithDb, getRecentSessionIdsWithDb } from './session';
 import { createStoryWithDb, createStoriesWithDb } from './story';
-import { createVotingResultWithDb, getAllVotingsWithDb } from './votingResult';
+import {
+  createVotingResultWithDb,
+  createVotingResultsWithDb,
+  getAllVotingsWithDb,
+} from './votingResult';
 
 const db = SQLite.openDatabase('db.db');
 
@@ -13,4 +17,5 @@ export const getRecentSessionIds = getRecentSessionIdsWithDb(db);
 export const createStory = createStoryWithDb(db);
 export const createStories = createStoriesWithDb(db);
 export const createVotingResult = createVotingResultWithDb(db);
+export const createVotingResults = createVotingResultsWithDb(db);
 export const getAllVotings = getAllVotingsWithDb(db);
---
+++ tasks/fetch/pastSessions.js
@@ -0,0 +1,40 @@
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+import {
+  createStories,
+  createVotingResults,
+  getRecentSessionIds,
+} from '../../database';
+import httpClient from '../../api/httpClient';
+
+const TASK_NAME = 'FETCH_PAST_SESSIONS';
+
+const updateSessions = async sessions => {
+  const stories = sessions
+    .flatMap(session => session.votings)
+    .map(voting => voting.story)
+    .map(story => ({ id: story.id, summary: story.summary }));
+  await createStories(stories);
+
+  const votingResults = sessions.flatMap(session =>
+    session.votings.map(voting => ({
+      sessionId: session.sessionId,
+      storyId: voting.story.storyId,
+      resultCard: voting.resultCard,
+    }))
+  );
+  await createVotingResults(votingResults);
+};
+
+TaskManager.defineTask(TASK_NAME, async () => {
+  try {
+    const sessionIdsToUpdate = await getRecentSessionIds();
+    const sessionsToUpdate = await httpClient.get(`/poker-sessions`, {
+      params: { sessionIds: sessionIdsToUpdate },
+    });
+    await updateSessions(sessionsToUpdate);
+    return BackgroundFetch.Result.NewData;
+  } catch (error) {
+    return BackgroundFetch.Result.Failed;
+  }
+});
